f5a1013ac10f4c97483e2e166d306a47bac1c78a
Brianna <brosem42@gmail.com>
2026-01-03 15:48:45 -0500

    update severity

--- src/app.js
+++ src/app.js
@@ -16,6 +16,62 @@ function App() {
     useEffect(() => {
         fetchDefects()
     }, []);
-    
 
-               
+    const fetchDefects = async () => {
+        try {
+            const response = await Client.graphql({
+                query: `query FinalTest {
+                    listDefects {
+                        id
+                        molding_machine_id 
+                        object_detection {
+                            reject
+                            contamination_defect {
+                                pixel_severity {
+                                    value
+                                }
+                            }
+                        }
+                    }
+                }`
+            }); 
+            const rawData = response.data.listDefects;
+            const flattened = rawData.map(item => ({
+                machine: item.molding_machine_id,
+                reject: item.object_detection?.reject ? 1 : 0,
+                severity: item.object_detection?.contamination_defect?.pixel_severity?.value || 0
+            }));
+
+            setData(flattened);
+        } catch (error) {
+            console.error("Error fetching defects:", error);
+        }
+    };
+
+    return (
+        <div style={{ height: "500px", width: "100%", padding: "20px" }}>
+            <h2>Defects analytics dashboard</h2>
+            {data.length > 0 ? (
+                <ResponsiveBar
+                    data={data}
+                    keys={['reject', 'severity']}
+                    indexBy="machine"
+                    margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+                    padding={0.3}
+                    valueScale={{ type: "linear" }}
+                    colors= {{ scheme: "nivo" }}
+                    legendLabel={d => `${d.id}`}
+                    axisBottom={{
+                        legend: "Machine ID",
+                        legendPosition: "middle",
+                        legendOffset: 40
+                    }}
+                />
+                ) : (
+                    <p>Connecting to DocumentDB via AppSync...</p>
+                )}
+        </div>
+    );
+}
+
+export default App;
